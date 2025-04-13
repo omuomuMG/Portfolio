0a873d3f76c4221239fac3dc3b180731539ef106
omuomuMG <omuomunotrice@gmail.com>
2025-04-13 22:31:01 +0900

    Feature: Modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Glob from "./features/Top/Top";
+import TopPage from "./features/Top/TopPage";
+import Content from "./features/Content/Content";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Glob />
+      <div>
+        <TopPage />
+        <Content />
+      </div>
     </div>
   );
 }
