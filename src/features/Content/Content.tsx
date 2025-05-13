68c5eda56505b20cd1d3871871e4177d6bccd453
omuomuMG <omuomunotrice@gmail.com>
2025-05-13 22:42:50 +0900

    Add: DeepL and Google Translator

--- src/features/Content/Content.tsx
+++ src/features/Content/Content.tsx
@@ -37,7 +37,7 @@ const contents: ContentItem[] = [
     imageUrl:
       "https://github.com/user-attachments/assets/515717dc-bd37-4d88-af7f-788386b3cb45",
     description:
-      "Ankiのアドオンの英単語の発音記号を自動的に出力するツールです。具体的には、入力された英単語に対して、オープンソースのCMU発音辞書を利用して発音情報を取得し、それを国際音声記号（IPA）に変換して表示します。これにより、ユーザーは英単語とその正確な発音記号を同時に学習でき、より効率的に語彙力を高めることができます。\nAnkiは言語学習者の間で非常に人気のあるアプリですが、発音記号を簡単に確認できる拡張機能はこれまで存在していませんでした。そこで私は、世界中の英語学習者の役に立ちたいという思いからこのプロジェクトを立ち上げました。\n現在、この拡張機能は100件以上ダウンロードされており、多くの学習者に利用されています。ユーザーから寄せられたフィードバックを積極的に取り入れ、継続的に改善を行っている点も、このプロジェクトの強みのひとつです。",
+      "Ankiのアドオンの英単語の発音記号を自動的に出力するツールです。具体的には、入力された英単語に対して、オープンソースのCMU発音辞書を利用して発音情報を取得し、それを国際音声記号（IPA）に変換して表示します。これにより、ユーザーは英単語とその正確な発音記号を同時に学習でき、より効率的に語彙力を高めることができます。\nAnkiは言語学習者の間で非常に人気のあるアプリですが、発音記号を簡単に確認できる拡張機能はこれまで存在していませんでした。そこで私は、世界中の英語学習者の役に立ちたいという思いからこのプロジェクトを立ち上げました。技術の選定理由としては、Ankiのアドオン開発で推薦されているPython, PyQtを軸に実装をしました。サードパーティライブラリの利用は基本的にできないため、ライブラリなどの力を借りずに限られた環境で開発を行うようにしました。\n現在、この拡張機能は100件以上ダウンロードされており、多くの学習者に利用されています。ユーザーから寄せられたフィードバックを積極的に取り入れ、継続的に改善を行っている点も、このプロジェクトの強みのひとつです。",
     techStack: ["Python", "PyQt", "CSS"],
     downloadUrl: "https://ankiweb.net/shared/info/2075109613?cb=1735716386693",
     githubUrl: "https://github.com/omuomuMG/Pronounce-Symbol-Generator",
@@ -45,41 +45,28 @@ const contents: ContentItem[] = [
   },
   {
     id: 3,
-    title: "Greatest Translator",
-    contentInfo: "Comming Soon",
+    title: "DeepL and Google Translator",
+    contentInfo: "ワンクリックで翻訳をする",
     imageUrl:
-      "https://github.com/user-attachments/assets/515717dc-bd37-4d88-af7f-788386b3cb45",
+      "https://github.com/user-attachments/assets/d97d4cfa-682f-4bf7-9884-f68bb311affa",
     description:
-      "Ankiのアドオンの英単語の発音記号を自動的に出力するツールです。具体的には、入力された英単語に対して、オープンソースのCMU発音辞書を利用して発音情報を取得し、それを国際音声記号（IPA）に変換して表示します。これにより、ユーザーは英単語とその正確な発音記号を同時に学習でき、より効率的に語彙力を高めることができます。\nAnkiは言語学習者の間で非常に人気のあるアプリですが、発音記号を簡単に確認できる拡張機能はこれまで存在していませんでした。そこで私は、世界中の英語学習者の役に立ちたいという思いからこのプロジェクトを立ち上げました。\n現在、この拡張機能は100件以上ダウンロードされており、多くの学習者に利用されています。ユーザーから寄せられたフィードバックを積極的に取り入れ、継続的に改善を行っている点も、このプロジェクトの強みのひとつです。",
+      "翻訳をワンクリックで行うことができるAnkiのアドオンです。DeepLとGoogle翻訳のAPIを利用して、選択したテキストを即座に翻訳します。これにより、学習中の単語やフレーズをすぐに理解でき、効率的な学習が可能になります。\n\nこのアドオンは特に語学学習者にとって便利で、Ankiのフラッシュカードを使用している際に、わからない単語やフレーズが出てきた場合にすぐに翻訳結果を得ることができます。これにより、学習の流れを中断することなく、スムーズに進めることができます。\n\n使用技術はPythonとPyQtで、シンプルで直感的なUIを提供しています。サードパーティライブラリとしては、DeepLライブラリ、google-api-python-clientライブラリ、dateutilを使用しています。Ankiでは、サードパーティのライブラリのサポートがないです。そのため最低限のライブラリを使い、ライブラリと共に配布しています。",
     techStack: ["Python", "PyQt", "CSS"],
-    downloadUrl: "https://ankiweb.net/shared/info/2075109613?cb=1735716386693",
-    githubUrl: "https://github.com/omuomuMG/Pronounce-Symbol-Generator",
-    statistics: "Downloads: 100+",
+    downloadUrl: "https://ankiweb.net/shared/info/259448931",
+    githubUrl: "https://github.com/omuomuMG/DeepL-and-Google-Translator",
+    statistics: "Comming Soon",
   },
+
   {
     id: 4,
-    title: "Apotify",
-    contentInfo: "Comming Soon",
-    imageUrl:
-      "https://github.com/user-attachments/assets/515717dc-bd37-4d88-af7f-788386b3cb45",
-    description:
-      "Ankiのアドオンの英単語の発音記号を自動的に出力するツールです。具体的には、入力された英単語に対して、オープンソースのCMU発音辞書を利用して発音情報を取得し、それを国際音声記号（IPA）に変換して表示します。これにより、ユーザーは英単語とその正確な発音記号を同時に学習でき、より効率的に語彙力を高めることができます。\nAnkiは言語学習者の間で非常に人気のあるアプリですが、発音記号を簡単に確認できる拡張機能はこれまで存在していませんでした。そこで私は、世界中の英語学習者の役に立ちたいという思いからこのプロジェクトを立ち上げました。\n現在、この拡張機能は100件以上ダウンロードされており、多くの学習者に利用されています。ユーザーから寄せられたフィードバックを積極的に取り入れ、継続的に改善を行っている点も、このプロジェクトの強みのひとつです。",
-    techStack: ["Python", "PyQt", "CSS"],
-    downloadUrl: "https://ankiweb.net/shared/info/2075109613?cb=1735716386693",
-    githubUrl: "https://github.com/omuomuMG/Pronounce-Symbol-Generator",
-    statistics: "Downloads: 100+",
-  },
-  {
-    id: 5,
     title: "Senryu",
     contentInfo: "今日の思いを川柳に込めて",
     imageUrl:
-      "https://private-user-images.githubusercontent.com/87554257/418465320-bd7202d0-399d-400e-8a80-039eb8b33791.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDY3ODU3ODgsIm5iZiI6MTc0Njc4NTQ4OCwicGF0aCI6Ii84NzU1NDI1Ny80MTg0NjUzMjAtYmQ3MjAyZDAtMzk5ZC00MDBlLThhODAtMDM5ZWI4YjMzNzkxLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA1MDklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNTA5VDEwMTEyOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTAzZjdhOGEzNWE2YjQyNTM0MTJhMGRlODFmY2EwNTA3MDRkYjc5YWU1NDQ3ZjcxZmNmMTc0Yjc3OTVkOGRlY2QmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.Csq7PJs3LyCzG7P8NW3vw0CBJTXsZHt7MP__muVbUNU",
+      "https://github.com/user-attachments/assets/ed67393c-5d2e-4c74-927a-8bd95c4e853d",
     description:
       "Ankiのアドオンの英単語の発音記号を自動的に出力するツールです。具体的には、入力された英単語に対して、オープンソースのCMU発音辞書を利用して発音情報を取得し、それを国際音声記号（IPA）に変換して表示します。これにより、ユーザーは英単語とその正確な発音記号を同時に学習でき、より効率的に語彙力を高めることができます。\nAnkiは言語学習者の間で非常に人気のあるアプリですが、発音記号を簡単に確認できる拡張機能はこれまで存在していませんでした。そこで私は、世界中の英語学習者の役に立ちたいという思いからこのプロジェクトを立ち上げました。\n現在、この拡張機能は100件以上ダウンロードされており、多くの学習者に利用されています。ユーザーから寄せられたフィードバックを積極的に取り入れ、継続的に改善を行っている点も、このプロジェクトの強みのひとつです。",
-    techStack: ["Python", "PyQt", "CSS"],
+    techStack: ["Next.js", "React", "TypeScript"],
     githubUrl: "https://github.com/omuomuMG/Senryu",
-    statistics: "Downloads: 100+",
   },
 ];
 
