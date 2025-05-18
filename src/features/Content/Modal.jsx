ace8f38c162b0d401103d503ee44cfe927a6a775
omuomuMG <omuomunotrice@gmail.com>
2025-05-18 14:27:11 +0900

    Change: モーダルのデザインをダークテーマに変更

--- src/features/Content/Modal.jsx
+++ src/features/Content/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import styles from "./Modal.module.css";
-
+import { IoMdDownload, IoLogoGithub } from "react-icons/io";
+import { TbWorldWww } from "react-icons/tb";
 
 const Modal = ({ showFlag, setShowModal, props }) => {
   useEffect(() => {
@@ -23,10 +24,15 @@ const Modal = ({ showFlag, setShowModal, props }) => {
       {showFlag ? (
         <div className={styles.overlay}>
           <div className={styles.modalContent}>
+            <img
+              className={styles.demo}
+              src={props.demo ? `${process.env.PUBLIC_URL}${props.demo}` : ""}
+              alt={props.demo}
+            />
             <h2 className={styles.sectionTitle}>説明</h2>
             <p className={styles.description}>{props.productDescription}</p>
-            <h2 className={styles.sectionTitle}>特徴</h2>
-            <p>{props.background}</p>
+            <h2 className={styles.sectionTitle}>開発背景</h2>
+            <p className={styles.description}>{props.background}</p>
             <h2 className={styles.sectionTitle}>使用技術</h2>
             <div className={styles.techStack}>
               {props.techStack &&
@@ -37,10 +43,13 @@ const Modal = ({ showFlag, setShowModal, props }) => {
                 ))}
             </div>
             <h2 className={styles.sectionTitle}>技術選定理由</h2>
-            <p>{props.techReason}</p>
-
-            <h2 className={styles.sectionTitle}>統計情報</h2>
-            <p>{props.statistics}</p>
+            <p className={styles.description}>{props.techReason}</p>
+            {props.statistics && (
+              <>
+                <h2 className={styles.sectionTitle}>統計情報</h2>
+                <p className={styles.description}>{props.statistics}</p>
+              </>
+            )}
             <h2 className={styles.sectionTitle}>リンク</h2>
             {props.downloadUrl && (
               <p>
@@ -50,6 +59,7 @@ const Modal = ({ showFlag, setShowModal, props }) => {
                   rel="noopener noreferrer"
                   className={styles.link}
                 >
+                  <IoMdDownload />
                   ダウンロード
                 </a>
               </p>
@@ -62,7 +72,21 @@ const Modal = ({ showFlag, setShowModal, props }) => {
                   rel="noopener noreferrer"
                   className={styles.link}
                 >
+                  <IoLogoGithub />
                   GitHub
+                </a>
+              </p>
+            )}
+            {props.siteUrl && (
+              <p>
+                <a
+                  href={props.siteUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.link}
+                >
+                  <TbWorldWww />
+                  サイト
                 </a>
               </p>
             )}
